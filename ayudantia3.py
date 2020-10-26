ba43148185a0f81ef2219d86c154db24e6801b28
David Medel <david.medel@uoh.cl>
2020-10-26 17:34:19 -0300

    update archivo

--- ayudantia3.py
+++ ayudantia3.py
@@ -39,7 +39,8 @@ class Punto:
         print("El vector entre {} y {} es ({},{})".format(self, p, nuevo_x, nuevo_y))
 
     def distancia(self, p):
-        d = math.sqrt((p.x - self.x)**2+(p.y - self.y)**2) #float
+        #distancia a la cuarta
+        d = math.sqrt((p.x - self.x)**4+(p.y - self.y)**4) #float
         print("La distancia entre los puntos {} y {} es {}".format(self, p, d))
 
 class Rectangulo:
